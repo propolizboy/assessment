ecd32e1a055bd9f6ea37e67f48ac606b9af00b1e
propolizboy <propolizboy@gmail.com>
2023-01-04 22:57:46 +0700

    setup Graceful shutdown

--- server.go
+++ server.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -15,5 +21,27 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	log.Println("Server started at:", port)
-	log.Fatal(e.Start(addr))
+
+	go func() {
+		if err := e.Start(addr); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	// graceful shutdown
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+	errSignal := <-shutdown
+	switch errSignal {
+	case os.Interrupt:
+		fmt.Println("Server Interrupt...")
+	case syscall.SIGTERM:
+		fmt.Println("Server Sigterm...")
+	}
+	fmt.Println("App is shutting down...")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal("Error shutting down: %v\n", err)
+	}
 }
