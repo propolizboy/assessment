a5266aa10812934df3e9d47e704beb1e06375ae8
propolizboy <propolizboy@gmail.com>
2023-01-06 09:26:49 +0700

    add Create data expense , add integration test

--- handler/routes.go
+++ handler/routes.go
@@ -12,4 +12,5 @@ func (h *Handler) gethealthHandler(c echo.Context) error {
 
 func (h *Handler) SetupRoute(e *echo.Echo) {
 	e.GET("/healths", h.gethealthHandler)
+	e.POST("/expenses", h.CreateExpenseHandler)
 }
